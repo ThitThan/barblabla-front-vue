488db6fbe35cf8587d4e92d3d66fb1014bab2608
Dive <kr.sannarong@gmail.com>
2019-05-05 14:09:37 +0700

    Home, ใส่ Loop ไปนิดหน่อย ชมกูดิ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,13 +9,25 @@
     <!-- งง ไอ้สัส นั่งทำความเข้าใจอยู่ได้ป่ะ get get ควยไร งง แม่ง ไปต่อกับ db ได้ไง เหี้ยแล้วกุต้องเช็คคอนดิชั่นไงเนี่ย หน่กหส่ฟหากสฟก -->
 
       <!-- หัวตาราง -->
-        <li class='collection-item waves-effect waves-light'>
+        <li class='collection-item'>
           <div class="row">
             <!-- <div class='col s12'>This div is 12-columns wide on all screen sizes</div> -->
             <div class="col s2">เลขโต๊ะ</div>
             <div class="col s3">รายชื่อลูกค้า</div>
             <div class="col s3">สถานะ</div>
             <div class="col s2">โซนการนั่ง</div>
+            <div class="col s2">-</div>
+          </div>
+
+          <!-- แต่ละแถว -->
+          <div class='row  waves-effect waves-light'
+          v-for='t in table' 
+          v-bind:key='t.Reserve'>
+            <!-- <div class='col s12'>This div is 12-columns wide on all screen sizes</div> -->
+            <div class="col s2">{{ t.get('TableNumber') }}</div> 
+            <div class="col s3">รายชื่อลูกค้า</div>
+            <div class="col s3">สถานะ</div>
+            <div class="col s2">{{t.get('Zone')}}</div>
             <div class="col s2">-</div>
           </div>
         </li>
