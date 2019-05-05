9c4d5dc338e11ad472f73af878cd109f1ee7074b
yawpraxg <prangkwan.br6231@gmail.com>
2019-05-05 18:31:17 +0700

    add dropdown

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,21 +5,29 @@
    <!-- display current date and time -->
     <span>{{ moment().format('Do MMMM YYYY, h:mm:ss a') }}</span>
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+  <center>
+     <vue-dropdown
+      :config="config"
+      @setSelectedOption="setNewSelectedOption($event);"
+    ></vue-dropdown>
+  </center>
+  
+       
 
-    <ul class="collection" v-if='table.length > 0'>
 
-    <!-- งง ไอ้สัส นั่งทำความเข้าใจอยู่ได้ป่ะ get get ควยไร งง แม่ง ไปต่อกับ db ได้ไง เหี้ยแล้วกุต้องเช็คคอนดิชั่นไงเนี่ย หน่กหส่ฟหากสฟก -->
+    <ul class="collection">
+      <!-- งง ไอ้สัส นั่งทำความเข้าใจอยู่ได้ป่ะ get get ควยไร งง แม่ง ไปต่อกับ db ได้ไง เหี้ยแล้วกุต้องเช็คคอนดิชั่นไงเนี่ย หน่กหส่ฟหากสฟก -->
 
       <!-- หัวตาราง -->
-        <li class='collection-item'>
-          <div class="row">
-            <!-- <div class='col s12'>This div is 12-columns wide on all screen sizes</div> -->
-            <div class="col s2">เลขโต๊ะ</div>
-            <div class="col s3">รายชื่อลูกค้า</div>
-            <div class="col s3">สถานะ</div>
-            <div class="col s2">โซนการนั่ง</div>
-            <div class="col s2">-</div>
-          </div>
+      <li class="collection-item">
+        <div class="row">
+          <!-- <div class='col s12'>This div is 12-columns wide on all screen sizes</div> -->
+          <div class="col s2">เลขโต๊ะ</div>
+          <div class="col s3">รายชื่อลูกค้า</div>
+          <div class="col s3">สถานะ</div>
+          <div class="col s2">โซนการนั่ง</div>
+          <div class="col s2">-</div>
+        </div>
 
           <!-- แต่ละแถว -->
           <div class='row  waves-effect waves-light'
@@ -53,33 +61,64 @@
       </ul>
   
   </div>
+  
 </template>
+
+
 
 <script>
 import Parse from "parse";
+import vueDropdown from "@/components/vue-dropdown/vue-dropdown";
+
 var Tableja = Parse.Object.extend("Tableja");
 var Reservation = Parse.Object.extend("Reservation");
 
 
 export default {
+  components: {
+    vueDropdown
+  },
   data() {
     return {
       isLoading: false,
       name: "dsdasd",
+      table: [],
 
       table: [],
       reservation: {
         // 'tableID': 'reserve'
-      },
+      // priao
+      config: {
+        options: [
+          {
+            value: "แสดงทั้งหมด"
+          },
+          {
+            value: "ว่าง"
+          },
+          {
+            value: "จองแล้ว"
+          }
+          ],
+          placeholder: "แสดงที่นั่งภายในร้าน",
+          backgroundColor: "#48494d",
+          textColor: "white",
+          borderRadius: "1em",
+          border: "1px solid gray",
+          width: 350
+        },
+      }
     };
   },
   created() {
     this.name = "5555";
     this.data_load();
   },
+
   methods: {
     hello() {
       alert("Hello!");
+      
     },
     methods: {
   moment: function () {
@@ -110,6 +149,7 @@ export default {
 
       this.table = tables;
     }
+    
   }
 };
 </script>
