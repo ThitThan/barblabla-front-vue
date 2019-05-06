b02f1c2b8c9ff9e7af57fa3955d9da39a7e0e7c1
hope <hope11981@hotmail.com>
2019-05-06 20:51:57 +0700

    Customer length person
    brabrabra

--- src/views/manage/Customer.vue
+++ src/views/manage/Customer.vue
@@ -1,23 +1,27 @@
 
 <template>
   <div class="container">
-    <h3>จัดการลูกค้า</h3>
+    <h3><i class="material-icons">portrait</i> จัดการลูกค้า</h3> 
+    <h6>{{ customer.length }} คน</h6>
 
     <!-- <input placeholder="รายซื่อลูกค้า" v-model='name'> -->
 
-    <!-- <div>{{ name }}</div> -->
+ <!-- <div>{{ name }}</div> -->
+   
     <div class="row">
-      <div class="col s6">ชื่อลูกค้า</div>
-      <div class="col s6">เบอร์โทร</div>
+      <div class="col s6">  ชื่อลูกค้า</div>
+      <div class="col s6">  เบอร์โทร</div>
        
     </div>
       
     <ul class="collection" >
         <li class="collection-item row" v-for="c in customer" v-bind:key='c.id'>
-            <div class="col s6"> {{ c.get("name") }} </div>
-
-
-             <div class="col s6">{{ c.get("phone") }}</div>
+            <div class="col s4">{{ c.get("name") }} 
+            <div class="col s6"><i class="material-icons">person</i>
+            </div></div>
+             <div class="col s6">{{ c.get("phone") }}
+            <div class="col s9"><i class="material-icons">phone</i>
+             </div></div>
             
         </li>
     </ul>
