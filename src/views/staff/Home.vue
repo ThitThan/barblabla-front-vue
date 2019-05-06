b72285ffff37061089c2aa98efb00e408493539a
Dive <kr.sannarong@gmail.com>
2019-05-06 19:56:53 +0700

    home nav app

--- src/views/staff/Home.vue
+++ src/views/staff/Home.vue
@@ -1,36 +1,43 @@
 
 <template>
-  <div class="about container">
-    <a class='waves-effect waves-light btn-small deep-purple darken-2' 
-      @click="performLogout()">
-      ล้อคเอาท์
-    </a>
-
-    <h3>This is the staff page</h3>
-    
-    <div style="margin: 24px 0px; height: 35px;">
-      <center style="z-index: 1000; position: absolute; margin-left: auto; margin-right: auto; left: 0px; right: 0px;">
-        <vue-dropdown :config="config" @setSelectedOption="setNewSelectedOption($event);"></vue-dropdown>
-      </center>
+  <div class>
+    <div class="nav-dark z-depth-1 valign-wrapper">
+      <img src="@/assets/refeel-logo.png" style="height: 36px">
+          <a
+      class="waves-effect waves-light btn-small deep-purple darken-2" 
+      @click="performLogout()"
+    >ล้อคเอาท์</a>
     </div>
 
-    <input placeholder="ชื่อ" v-model='name'>
+    <div class="about container">
+      <h3>This is the staff page</h3>
 
-    <div>{{ name }}</div>
-    
+      <div style="margin: 24px 0px; height: 35px;">
+        <center
+          style="z-index: 1000; position: absolute; margin-left: auto; margin-right: auto; left: 0px; right: 0px;"
+        >
+          <vue-dropdown :config="config" @setSelectedOption="setNewSelectedOption($event);"></vue-dropdown>
+        </center>
+      </div>
+
+      <input placeholder="ชื่อ" v-model="name">
+
+      <div>{{ name }}</div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import vueDropdown from "@/components/vue-dropdown/vue-dropdown";
-import Parse from 'parse'
+import Parse from "parse";
 export default {
   components: {
     vueDropdown
   },
   data() {
     return {
-      name: '...',
+      name: "...",
       displayAvailable: true,
       displayReserved: true,
       config: {
@@ -51,21 +58,21 @@ export default {
         borderRadius: "1em",
         border: "1px solid gray",
         width: 300
-      },
-    }
+      }
+    };
   },
   created() {
-    this.name = '5555'
+    this.name = "5555";
   },
   methods: {
     performLogout() {
       Parse.User.logOut().then(() => {
-        this.$router.push({ path: '/login' })
-      })
+        this.$router.push({ path: "/login" });
+      });
     },
     hello() {
-      alert('Hello!')
+      alert("Hello!");
     }
   }
-}
+};
 </script>
